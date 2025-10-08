0528ed4d3e7cc7db4f64f00f5ebb50cdb45fc110
Ivan Luka Špoljar <lukispoljar@gmail.com>
2025-10-08 23:41:59 +0200

    new_design

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
 import './App.scss';
 
 function App() {
@@ -7,36 +8,38 @@ function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
+  /* Get and set current time in string format hh:mm:ss */
+  const currentTime = () => {
+    let dateNow: Date = new Date(Date.now());
+
+    let hours: number | string = dateNow.getHours();
+    let minutes: number | string = dateNow.getMinutes();
+    let seconds: number | string = dateNow.getSeconds();
+
+    const turnToString = (time: number) => time < 10 ? '0' + time.toString() : time.toString();
+
+    hours = turnToString(hours);
+    minutes = turnToString(minutes);
+    seconds = turnToString(seconds);
+
+    setTimeToDisplay(`${hours}:${minutes}:${seconds}`);
+  }
+
+  /* Fetch and set quote as string */
+  async function fetchQuote() {
+    try {
+      const res = await fetch("https://quoteslate.vercel.app/api/quotes/random");
+      return await res.json();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
 
-    /* Get and set current time in string format hh:mm:ss */
-    const currentTime = () => {
-      let dateNow: Date = new Date(Date.now());
-
-      let hours: number | string = dateNow.getHours();
-      let minutes: number | string = dateNow.getMinutes();
-      let seconds: number | string = dateNow.getSeconds();
-
-      const turnToString = (time: number) => time < 10 ? '0' + time.toString() : time.toString();
-
-      hours = turnToString(hours);
-      minutes = turnToString(minutes);
-      seconds = turnToString(seconds);
-
-      setTimeToDisplay(`${hours}:${minutes}:${seconds}`);
-    }
     currentTime();
     setInterval(currentTime, 1000);
 
-    /* Fetch and set quote as string */
-    async function fetchQuote() {
-      try {
-        const res = await fetch("https://quoteslate.vercel.app/api/quotes/random");
-        return await res.json();
-      } catch (error) {
-        console.log(error)
-      }
-    }
     fetchQuote().then(data => {
       setAuthor(data.author);
       setQuote(data.quote);
@@ -45,16 +48,26 @@ function App() {
   }, []);
 
   return (
-    <>
+    <div id='app-wrapper'>
       <div id='time-wrapper'>
-        <p id='time'>{timeToDisplay}</p>
+        <p id='time' className='glow-text'>{timeToDisplay}</p>
+        <Button variant="contained" onClick={(event: any) => fetchQuote().then(data => {
+          setAuthor(data.author);
+          setQuote(data.quote);
+        })}>
+          <strong>Next quote</strong>
+        </Button>
       </div>
 
       <div id='quote-wrapper'>
-        <p id='quote-author'>{author}</p>
-        <p id='quote'>{quote}</p>
+        <div id='quote-card'>
+          <p id='quote'><strong>&ldquo;{quote}&rdquo;</strong></p>
+          <p id='quote-author'>{author}</p>
+        </div>
       </div>
-    </>
+
+
+    </div>
   );
 }
 
